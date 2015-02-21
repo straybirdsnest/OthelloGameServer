b53342e253ec55d0c880316fdcc667caf7e5c04f
stryabirdsnest <straybirdsnest@gmail.com>
2015-02-21 21:37:07 +0800

    修改配置文件中MySQL数据库的用户名与密码，以正确连接数据库。

--- src/otakuplus/straybird/OthelloGameServer.java
+++ src/otakuplus/straybird/OthelloGameServer.java
@@ -1,7 +1,7 @@
 package otakuplus.straybird;
 
 // mysql database name othellogamedb
-// username othello@localhost
+// username othelloserver@localhost
 // password 8ae8e9f2af3b29f473d440306ad14faf
 
 public class OthelloGameServer {
