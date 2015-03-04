c988750ce933a23fb46b86159a4878fb25c3ed72
stryabirdsnest <straybirdsnest@gmail.com>
2015-03-04 12:57:47 +0800

    增加服务器Response的Request记录，以供查询

--- src/otakuplus/straybird/othellogameserver/network/ProcessResponse.java
+++ src/otakuplus/straybird/othellogameserver/network/ProcessResponse.java
@@ -1,8 +1,22 @@
 package otakuplus.straybird.othellogameserver.network;
 
 public class ProcessResponse {
+	public static int LOGIN = 100;
+	public static int LOGOUT = 101;
+	public static int GET_USER_INFO = 102;
+	public static int GET_USER_ONLINE_LIST = 103;
+
+	private int requestType;
 	private boolean responseState = false;
-	
+
+	public int getRequestType() {
+		return requestType;
+	}
+
+	public void setRequestType(int requestType) {
+		this.requestType = requestType;
+	}
+
 	public boolean isResponseState() {
 		return responseState;
 	}
