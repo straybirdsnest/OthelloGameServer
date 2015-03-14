2932095fe419f3e5710d25e221bbc74569fa5cdd
stryabirdsnest <straybirdsnest@gmail.com>
2015-03-14 17:58:58 +0800

    新增与数据库中OthelloGameTable表对应的GameTable类与配置文件 修改GameRecord配置文件中对应数据库表的名称

---
+++ src/otakuplus/straybird/othellogameserver/model/GameTable.java
@@ -0,0 +1,37 @@
+package otakuplus.straybird.othellogameserver.model;
+
+public class GameTable {
+
+	private int gameTableId;
+	private int playerAId;
+	private int playerBId;
+
+	public int getGameTableId() {
+		return gameTableId;
+	}
+
+	public void setGameTableId(int gameTableId) {
+		this.gameTableId = gameTableId;
+	}
+
+	public int getPlayerAId() {
+		return playerAId;
+	}
+
+	public void setPlayerAId(int playerAId) {
+		this.playerAId = playerAId;
+	}
+
+	public int getPlayerBId() {
+		return playerBId;
+	}
+
+	public void setPlayerBId(int playerBId) {
+		this.playerBId = playerBId;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
